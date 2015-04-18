a81131357752265ab495dd2a96b1f9b083966ad4
Mikhail Karpenko <veegboo@gmail.com>
2015-04-17 20:27:00 -0600

    WIP: remove teardrops from selected tracks

--- pcbnew/pcbframe.cpp
+++ pcbnew/pcbframe.cpp
@@ -738,6 +738,7 @@ void  PCB_EDIT_FRAME::ShowTeardropsEditor( wxCommandEvent& event )
         SELECTION selection = m_toolManager->GetTool<SELECTION_TOOL>()->GetSelection();
         TEARDROPS_EDITOR editor(this, GetGalCanvas()->GetView());
         editor.EditTeardrops(selection, settings);
+        OnModify();
     }
 }
 
